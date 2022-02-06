74c51e1b62999d6b0ecf184a1422708fd6f53b48
oneku16 <eku.ulanov@yandex.com>
2022-02-07 02:45:33 +0600

    first commit

---
+++ pythonforpythoners/less_1_if_bool_loop.py
@@ -0,0 +1,73 @@
+# 06/02/2022
+
+
+# if
+
+# imhungry = False
+
+# if imhungry: print("feed")
+# else: print("i am happy!")
+
+
+# num = int(input("Enter: "))
+
+# num = str(num)
+# print(len(num))
+
+# if len(num) == 1:
+#     print("1")
+# elif len(num) == 2:
+#     print("2")
+# else:
+#     print("3<")
+# cnt = 0
+# for i in num:
+#     cnt += 1
+#     print(cnt)
+
+# print(cnt)
+
+# for i in range(1, 100, 2):
+#     print(i , end = " ")
+
+
+
+# n = int(input("Number: "))
+# for i in range(n):
+
+
+#     num = int(input("Enter ur num: "))
+#     if num % 2 == 0: print("this num is even")
+#     else: print("this is odd num")
+
+
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# n = 5
+
+# for i in range(1, 6):
+
+#     if i == 1:
+#         print(1)
+#     elif i == 2:
+#         print(1, 2)
+#     elif i == 3:
+#         print(1,2,3)
+#     elif i == 4:
+#         print(1, 2, 3, 4)
+#     elif i == 5:
+#         print(1,2,3,4,5)
+
+
+for i in range(1, 11):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+
